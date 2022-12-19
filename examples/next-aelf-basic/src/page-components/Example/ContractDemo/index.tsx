fef1b7f29a5d164ffd582588cebecce940ed291f
AbigailDeng <Abigail.deng@ienyan.com>
2022-12-19 10:48:11 +0800

    feat: add example

---
+++ examples/next-aelf-basic/src/page-components/Example/ContractDemo/index.tsx
@@ -0,0 +1,97 @@
+import { Card, Button } from 'antd';
+import React, { useCallback, useEffect, useState } from 'react';
+import { AElfReactProvider, getSignatureByBridge, useAElfReact } from '@aelf-react/core';
+import AElf from 'aelf-sdk';
+import { checkAElfBridge } from 'utils/checkAElfBridge';
+function ContractApp() {
+  // defaultAElfBridge is the first node, which is tDVW
+  const { account, activate, deactivate, pubKey, defaultAElfBridge, aelfBridges } = useAElfReact();
+  const [blockHeight, setBlockHeight] = useState<any>();
+  const [contract, setContract] = useState<any>();
+  const [tokenInfo, setTokenInfo] = useState<any>();
+  const [signature, setSignature] = useState<any>();
+  const changeWallet = useCallback(async () => {
+    await deactivate();
+    activate();
+  }, [activate, deactivate]);
+  useEffect(() => {
+    activate();
+  }, [activate]);
+  const childrenList = () => {
+    const list = [];
+    account && list.push(`account:${account}`);
+    pubKey && list.push(`pubKey:${pubKey}`);
+    blockHeight && list.push(`blockHeight:${JSON.stringify(blockHeight)}`);
+    contract && list.push(`contract methods:${JSON.stringify(Object.keys(contract))}`);
+    tokenInfo && list.push(`ELF tokenInfo:${JSON.stringify(tokenInfo)}`);
+    signature && list.push(`signature example:${signature}`);
+    return list;
+  };
+  return (
+    <>
+      {childrenList().map((i, k) => (
+        <p key={k}>{i}</p>
+      ))}
+      <button onClick={() => activate()}>activate</button>
+      <button onClick={() => deactivate()}>deactivate</button>
+      <button onClick={() => changeWallet()}>changeWallet</button>
+      <button
+        onClick={async () => {
+          if (!aelfBridges) return;
+          try {
+            // aelf-bridge only supports one node and needs to check whether it is connected
+            // NightElf does not require this function
+            await checkAElfBridge(aelfBridges.tDVW);
+            const tDVWReq = await aelfBridges.tDVW.chain.getBlockHeight();
+            // aelf-bridge returns the result directly NightElf will return the result in the result
+            setBlockHeight({
+              tDVWBlockHeight: tDVWReq.result || tDVWReq,
+            });
+          } catch (error) {
+            setBlockHeight(`${error}`);
+          }
+        }}>
+        getBlockHeight
+      </button>
+      <button
+        onClick={async () => {
+          if (!defaultAElfBridge || !account) return;
+          const req = await defaultAElfBridge.chain.contractAt('ASh2Wt7nSEmYqnGxPPzp4pnVDU4uhj1XW9Se5VeZcX2UDdyjx', {
+            address: account,
+          });
+          setContract(req.result || req);
+        }}>
+        contractAt Token contract
+      </button>
+      <button
+        onClick={async () => {
+          if (!contract) return;
+          const req = await contract.GetTokenInfo.call({ symbol: 'ELF' });
+          setTokenInfo(req.result || req);
+        }}>
+        GetTokenInfo
+      </button>
+      <button
+        onClick={async () => {
+          if (!defaultAElfBridge || !account) return;
+          const signature = await getSignatureByBridge(defaultAElfBridge, account, AElf.utils.sha256('example'));
+          setSignature(signature);
+        }}>
+        Signature
+      </button>
+    </>
+  );
+}
+export default function ContractDemo() {
+  return (
+    <Card title="get contract">
+      <AElfReactProvider
+        appName="example"
+        nodes={{
+          tDVW: { rpcUrl: 'https://tdvw-test-node.aelf.io', chainId: 'tDVW' },
+        }}>
+        <ContractApp></ContractApp>
+      </AElfReactProvider>
+    </Card>
+  );
+}
